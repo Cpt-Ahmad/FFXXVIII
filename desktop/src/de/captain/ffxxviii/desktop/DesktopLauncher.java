f55d3f71fcdc0df66ea4c13a7a267030254e9525
Cpt-Ahmad <34349578+Cpt-Ahmad@users.noreply.github.com>
2018-01-05 18:31:02 +0100

    Add files via upload

---
+++ desktop/src/de/captain/ffxxviii/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.captain.ffxxviii.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.captain.ffxxviii.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
